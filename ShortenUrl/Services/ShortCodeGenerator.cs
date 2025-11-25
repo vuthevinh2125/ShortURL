dd316e1725dc88441f9f7cf93c0318cedc1382a2
Vu The Vinh <vinhvu20102005@gmail.com>
2025-11-25 12:57:25 +0700

    Initial commit and fixed .gitignore

---
+++ ShortenUrl/Services/ShortCodeGenerator.cs
@@ -0,0 +1,23 @@
+﻿// ShortenUrl/Services/ShortCodeGenerator.cs
+using Xunit; 
+using System.Linq;
+namespace ShortenUrl.Services
+{
+    public class ShortCodeGenerator
+    {
+        private const string Alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        private const int CodeLength = 7;
+
+        private readonly Random _random = new Random();
+
+        public string GenerateUniqueCode()
+        {
+            var code = new char[CodeLength];
+            for (int i = 0; i < CodeLength; i++)
+            {
+                code[i] = Alphabet[_random.Next(Alphabet.Length)];
+            }
+            return new string(code);
+        }
+    }
+}
